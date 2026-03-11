ddfdc109b936e9312120d6f61377542650e228f9
Michael <michaeelp.dev@gmail.com>
2026-03-10 22:21:58 -0300

    basic calculator in console

--- src/main.py
+++ src/main.py
@@ -1,1 +1,20 @@
-print("Python Calculator")
+import operations
+
+numberA = float(input("Enter the first number: "))
+numberB = float(input("Enter the second number: "))
+
+
+operation = input("Enter the operation: ")
+
+match operation:
+    case "+":
+        result = operations.add(numberA, numberB)
+    case "-":
+        result = operations.subtract(numberA, numberB)
+    case "*":
+        result = operations.multiply(numberA, numberB)
+    case "/":
+        result = operations.divide(numberA, numberB)
+    case "**":
+        result = operations.power(numberA, numberB)
+print(f"The result is: {result}")
